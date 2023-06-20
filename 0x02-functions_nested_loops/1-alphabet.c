92fd8bf558f14449fa84926b4097d30b4bd2dfea
ngumbau-ngina <ngumbaungina@gmail.com>
2023-06-20 13:17:31 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+* print_alphabet - Print the alphabet in lowercase.
+*
+* Return: void.
 */
 void print_alphabet(void)
 {
