0a79a789c92fe17fec442b4075c8bc015ac100cb
ngumbau-ngina <ngumbaungina@gmail.com>
2023-06-22 15:33:25 -0700

    print line

---
+++ positive_or_negative.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (n > 0)
+		printf("%d is positive\n", i);
+	else if (n < 0)
+		printf("%d is negative\n", i);
+	else (n = 0)
+		printf("%d is zero\n", i);
+return (0);
+}
