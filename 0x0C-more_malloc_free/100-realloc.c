9cdd29826aa30767403ce0ea4cc1ed840d670f73
ngumbau-ngina <ngumbaungina@gmail.com>
2023-07-13 12:15:04 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,54 +1,51 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
+#include "main.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
+ * *_realloc -  reallocates a memory block using malloc and free
+ * @ptr: void pointer
+ * @old_size: already allocated size
+ * @new_size: new size to allocate
+ * Return: pointer to newly allocated memory or null
+ **/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned char *np;
 	unsigned int i;
 
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size * sizeof(void *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	np = malloc(new_size * sizeof(char));
+	if (np == NULL)
+		return (NULL);
 	i = 0;
-	while (i < size)
+	if (new_size > old_size)
 	{
-		if (i % 10)
-	{
-			printf(" ");
+		while (i < old_size)
+		{
+			np[i] = ((char *)ptr)[i];
+			i++;
+		}
+		free(ptr);
+		return (np);
 	}
-		if (!(i % 10) && i)
+/* if new_size < old_size */
+	while (i < new_size)
 	{
-			printf("\n");
-	}
-		printf("0x%02x", buffer[i]);
+		np[i] = ((char *)ptr)[i];
 		i++;
 	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *p;
-	int i;
-
-	p = malloc(sizeof(char) * 10);
-	p = realloc(p, sizeof(char) * 98);
-	i = 0;
-	while (i < 98)
-	{
-		p[i++] = 98;
-	}
-	simple_print_buffer(p, 98);
-	free(p);
-	return (0);
-}
+	free(ptr);
+	return (np);
+}}
