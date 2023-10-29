f42488b2eb01583a7c0ac229443a17244e3bb47e
ngumbau-ngina <ngumbaungina@gmail.com>
2023-10-29 03:48:57 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - writes its name
+ * @argc: argument count
+ * @argv: array of argument in the command line
+ * return:0
+ * author@ngumbau
+**/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
