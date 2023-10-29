0ea475547a64406bc988f505a0ce23c74a675f45
ngumbau-ngina <ngumbaungina@gmail.com>
2023-10-29 03:26:13 -0700

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - writes its name
+ * @argc: argument count
+ * @argv: array of arguments
+ * return:0
+ * author @ngumbau
+**/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", *argv);
+	return (0);
+}
+
